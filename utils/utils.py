298909162e50803c190eec1880a9a9183e04453a
Tinsae Wondimu Techanea <tinsuw7@gmail.com>
2022-05-28 14:42:15 +0200

    notebook with code only

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,21 @@
 
 import numpy as np
 
+def sigmoid(x):
+    """
+    Compute the sigmoid function for the input here.
+    Arguments:
+    x -- A scalar or numpy array.
+    Return:
+    s -- sigmoid(x)
+    """
+
+    ### YOUR CODE HERE (~1 Line)
+    s = 1/(1 + np.exp(-x))
+    ### END YOUR CODE
+
+    return s
+
 def normalizeRows(x):
     """ Row normalization function
 
